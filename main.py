85109513c3d3406170abecf5d93cf47f79086121
poormohammadf <poormohammadf@gmail.com>
2018-01-01 00:38:25 +0330

    changed

--- main.py
+++ main.py
@@ -6,13 +6,20 @@ from bottle import static_file, request, post , get , response
 from bson.objectid import ObjectId
 import hashlib
 
-@route('/')
-def home():
+config = {}
+
+def session_is_set():
+  global config
   cookie = request.get_cookie("user_id")
   if cookie:
     username = db.users.find_one({'_id':ObjectId(str(cookie))})
-    return template("Hello {{name}}. Welcome back.", name=username['username'])
-  return template('templates/index.html',title='Home Page',base='')
+    config['session'] = username['username']
+
+@route('/')
+def home():
+  global config
+  session_is_set()
+  return template('templates/index.html',title='Home Page',base='',config=config)
 
 """--------------load static files which are comprised of javascript and stylesheet and image--------------"""
 @route('/statics/css/<filename:re:.*\.css>')
@@ -26,26 +33,34 @@ def send_js(filename):
 
 @route('/user/register')
 def register():
-  return template('templates/register.html',param = {})
+  session_is_set()
+  return template('templates/register.html',config = config)
 
 @route('/user/login')
 def login():
-  return template('templates/login.html',param = {})
+  session_is_set()
+  return template('templates/login.html',config = config)
+
+@route('/user/logout')
+def logout():
+  response.delete_cookie('user_id', path='/')
+  return redirect("/")
 
 @post('/users/login/check')
 def login_check():
-    data = {}
+    global config
     _user = request.forms.get('username', None).lower()
     _pass = request.forms.get('password', None)
     check = db.users.find_one({'username': _user, 'password': hashlib.md5(str(_pass)).hexdigest()})
     if check:
         response.set_cookie('user_id', str(check['_id']), path='/')
         return redirect('/')
-    data['error'] = 'Authentication failed!'
-    return template('templates/login.html', param=data)
+    config['error'] = 'Authentication failed!'
+    return template('templates/login.html', config=config)
     
 @post('/register')
 def form_submit():
+  global config
   f = request.forms.get('firstname', None)
   l = request.forms.get('lastname', None)
   u = request.forms.get('username', None).lower()
@@ -65,15 +80,15 @@ def form_submit():
     #db['users'].insert_one(data)
     db.users.insert_one(data)
   except DuplicateKeyError:
-    data = {}
-    data['error'] = 'duplicate'
-    return template('templates/register.html', param=data)
+    config['error'] = 'duplicate'
+    return template('templates/register.html', config=config)
   return redirect("/user/show")
 
 @route('/user/show')
 def successful():
+  session_is_set()
   data = list(db.users.find().limit(10))
-  return template('templates/table.html', param=data)
+  return template('templates/table.html', param=data,config=config)
 
 
 if __name__ == '__main__':
